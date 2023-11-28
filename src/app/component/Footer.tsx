058b90c6caaf98ce242779ae5029b241109293ee
Abdullah <abdulluhodulate@gmail.com>
2023-11-28 05:26:02 +0100

    finishing touches

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -3,22 +3,22 @@ import React from "react";
 export default function Footer() {
 	return (
 		<footer className="bg-[#F6F6F6] mt-8 py-8 w-full">
-			<div className="flex flex-col md:flex-row max-w-[900px] mx-auto gap-20 text-center md:text-left">
+			<div className="flex flex-col justify-between md:flex-row max-w-[900px] gap-28 mx-auto text-center md:text-left">
 				<div>
-					<h2 className="font-bold uppercase">Digital Agency</h2>
-					<p className="max-w-[200px]">
+					<h2 className="font-bold uppercase text-xl pb-5">Digital Agency</h2>
+					<p className="max-w-[200px] mx-auto  md:mx-0">
 						Building digital products, brands & experience
 					</p>
 				</div>
-				<div className="grid md:grid-cols-3 gap-16">
+				<div className="grid md:grid-cols-3 gap-20">
 					<div>
 						<h3 className="font-bold text-xl pb-5">Resources</h3>
 
 						<ul>
-							<li className="py-3">Guides</li>
-							<li className="py-3">Blog</li>
-							<li className="py-3">Customer Stories</li>
-							<li className="py-3">Glossery</li>
+							<li className="pb-3">Guides</li>
+							<li className="pb-3">Blog</li>
+							<li className="pb-3">Customer Stories</li>
+							<li className="pb-3">Glossery</li>
 						</ul>
 					</div>
 
@@ -26,10 +26,10 @@ export default function Footer() {
 						<h3 className="font-bold text-xl pb-5">Company</h3>
 
 						<ul>
-							<li className="py-3">AboutUs</li>
-							<li className="py-3">Careers</li>
-							<li className="py-3">Partners</li>
-							<li className="py-3">Contact Us</li>
+							<li className="pb-3">AboutUs</li>
+							<li className="pb-3">Careers</li>
+							<li className="pb-3">Partners</li>
+							<li className="pb-3">Contact Us</li>
 						</ul>
 					</div>
 
@@ -37,10 +37,10 @@ export default function Footer() {
 						<h3 className="font-bold text-xl pb-5">Social Media</h3>
 
 						<ul>
-							<li className="py-3">LinkedIn</li>
-							<li className="py-3">Facebook</li>
-							<li className="py-3">Instagram</li>
-							<li className="py-3">Twitter</li>
+							<li className="pb-3">LinkedIn</li>
+							<li className="pb-3">Facebook</li>
+							<li className="pb-3">Instagram</li>
+							<li className="pb-3">Twitter</li>
 						</ul>
 					</div>
 				</div>
